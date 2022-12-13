e64e0eeee952550e484ae8d708c4ebd7a5ea3e8d
Maksym Kulibaba <Maksym.Kulibaba@rotary.org>
2022-12-13 23:32:20 +0200

    Add prettier, eslint, pre-commits

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  const atrt_tet = 0
-
   return (
     <div className='App'>
       <div>
@@ -17,6 +15,7 @@ function App() {
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
+
       <h1>Vite + React</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
